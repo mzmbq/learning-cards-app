f74733f86cde72f42fc5d8f44a2d0e45c0ed6c9c
mzmbq <mzmbq@proton.me>
2024-02-23 17:20:15 +0100

    Add config

--- backend/cmd/apiserver/main.go
+++ backend/cmd/apiserver/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/mzmbq/learning-cards-app/backend/internal/app/apiserver"
+)
+
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "../config.toml", "path to the config file")
+}
+
 func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
 	
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(config.BindAddr)
+	fmt.Println(config.LogLevel)
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
-module github.com/mzmbq/learning-cards-app
+module github.com/mzmbq/learning-cards-app/backend
 
 go 1.22.0
+
+require github.com/BurntSushi/toml v1.3.2
