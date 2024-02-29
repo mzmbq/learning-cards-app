d831d803c42379a55ad54e1f68676fc5b9016ba7
mzmbq <mzmbq@proton.me>
2024-02-29 19:23:28 +0100

    Add API routes

--- backend/internal/app/apiserver/server.go
+++ backend/internal/app/apiserver/server.go
@@ -6,12 +6,36 @@ import (
 )
 
 type server struct {
+	mux *http.ServeMux
 }
 
 func newServer() *server {
-	return &server{}
+	s := &server{
+		mux: http.NewServeMux(),
+	}
+
+	s.configureRouter()
+
+	return s
 }
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello")
+	s.mux.ServeHTTP(w, r)
+}
+
+func (s *server) configureRouter() {
+	s.mux.HandleFunc("GET /cards/", apiHandler)
+	s.mux.HandleFunc("GET /", rootHandler)
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "(ʘ ͜ʖ ʘ)")
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Hello")
 }
