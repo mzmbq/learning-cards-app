bdcc6cdf417e571d9b12333c69bd47af5a40b13b
mzmbq <mzmbq@proton.me>
2024-03-02 00:24:33 +0100

    Add `user` endpoint

--- backend/internal/app/apiserver/server.go
+++ backend/internal/app/apiserver/server.go
@@ -1,17 +1,26 @@
 package apiserver
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/mzmbq/learning-cards-app/backend/internal/app/model"
+	"github.com/mzmbq/learning-cards-app/backend/internal/app/store"
 )
 
 type server struct {
-	mux *http.ServeMux
+	mux   *http.ServeMux
+	store store.Store
 }
 
-func newServer() *server {
+func newServer(store store.Store) *server {
 	s := &server{
-		mux: http.NewServeMux(),
+		mux:   http.NewServeMux(),
+		store: store,
 	}
 
 	s.configureRouter()
@@ -26,6 +35,9 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (s *server) configureRouter() {
 	s.mux.HandleFunc("GET /cards/", apiHandler)
 	s.mux.HandleFunc("GET /", rootHandler)
+
+	s.mux.HandleFunc("POST /api/user/create/", s.handleUserCreate())
+	s.mux.HandleFunc("GET /api/user/{id}", s.handleUserFind())
 }
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,4 +50,47 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintf(w, "Hello")
+}
+
+func (s *server) handleUserCreate() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		dec := json.NewDecoder(r.Body)
+		var u model.User
+		if err := dec.Decode(&u); err != nil {
+			http.Error(w, "invalid json payload", http.StatusBadRequest)
+			return
+		}
+
+		if err := s.store.User().Create(&u); err != nil {
+			http.Error(w, "create user failed", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, u.ID)
+	}
+}
+
+func (s *server) handleUserFind() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		idStr := strings.TrimPrefix(r.URL.Path, "/api/user/")
+
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			http.Error(w, "invalid id", http.StatusBadRequest)
+			return
+		}
+
+		u, err := s.store.User().Find(id)
+		if err != nil {
+			http.Error(w, "user not found", http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(u); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
