88eecc91795c454fbc0a81abc2ab3e51e69c93aa
mzmbq <mzmbq@proton.me>
2024-03-03 13:15:59 +0100

    Fetch API in frontend

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from "react";
+
 type Props = {};
 
 export default function Card({}: Props) {
+  const [data, setData] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:8080/api/user/3");
+      const result = await response.text();
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
       <div>Front Text</div>
       <div>Back Text</div>
+      <div>User: {data}</div>
     </>
   );
 }
