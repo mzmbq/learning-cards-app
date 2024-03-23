aa120d838eccc6050334293c1fe91b90caeb41d0
mzmbq <mzmbq@proton.me>
2024-03-23 14:26:38 +0100

    Add Postgres DB

--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,5 @@ module github.com/mzmbq/learning-cards-app/backend
 go 1.22.1
 
 require github.com/BurntSushi/toml v1.3.2
+
+require github.com/lib/pq v1.10.9 // indirect
--- backend/internal/app/apiserver/apiserver.go
+++ backend/internal/app/apiserver/apiserver.go
@@ -1,15 +1,34 @@
 package apiserver
 
 import (
+	"database/sql"
 	"net/http"
 
-	"github.com/mzmbq/learning-cards-app/backend/internal/app/store/teststore"
+	_ "github.com/lib/pq"
+	"github.com/mzmbq/learning-cards-app/backend/internal/app/store/sqlstore"
 )
 
 func Start(config *Config) error {
-	store := teststore.New()
+	db, err := newDB(config.DatabaseURL)
+	if err != nil {
+		return err
+	}
+	store := sqlstore.New(db)
 
 	srv := newServer(store)
 
 	return http.ListenAndServe(config.BindAddr, srv)
+}
+
+func newDB(dbURL string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
