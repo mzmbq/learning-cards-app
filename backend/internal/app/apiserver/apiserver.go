8bd487dc5bdd60325df3d488a001dbaf737e0351
mzmbq <mzmbq@proton.me>
2024-05-04 15:54:44 +0200

    Change Postgres driver
    The lib/pq dirver ignores the context

--- backend/internal/app/apiserver/apiserver.go
+++ backend/internal/app/apiserver/apiserver.go
@@ -1,10 +1,13 @@
 package apiserver
 
 import (
+	"context"
 	"database/sql"
+	"log"
 	"net/http"
+	"time"
 
-	_ "github.com/lib/pq"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/mzmbq/learning-cards-app/backend/internal/app/store/sqlstore"
 )
 
@@ -21,14 +24,20 @@ func Start(config *Config) error {
 }
 
 func newDB(dbURL string) (*sql.DB, error) {
-	db, err := sql.Open("postgres", dbURL)
+	log.Println("Connecting to database")
+	db, err := sql.Open("pgx", dbURL)
 	if err != nil {
 		return nil, err
 	}
 
-	if err := db.Ping(); err != nil {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	if err := db.PingContext(ctx); err != nil {
+		db.Close()
+		log.Println("Failed to connect to database")
 		return nil, err
 	}
 
+	log.Println("Connected to database")
 	return db, nil
 }
