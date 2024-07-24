4e5fff692ce3bc0b45f85966ce1cc3545a0ef7da
mzmbq <mzmbq@proton.me>
2024-07-24 23:14:51 +0200

    Update frontend configuration to use environment-specific backend URLs

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,8 @@
 const CONFIG = {
-    backendURL: "http://localhost:8080",
+  backendURL:
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_BACKEND_URL_DEVELOPMENT
+      : process.env.REACT_APP_BACKEND_URL_PRODUCTION,
 };
 
 export default CONFIG;
