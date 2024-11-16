3805de83bdb3a03340ba36cfa30d34541b7fc0b0
mzmbq <mzmbq@proton.me>
2024-11-16 19:23:47 +0100

    Update `package.json` to use the correct backend URL

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replaces PLACEHOLDER_URL with the actual backend URL
-# TODO: find better solution for this
+# TODO: Find better solution for this
 sed -i "s|https://api.example.com|${REACT_APP_BACKEND_URL_PRODUCTION}|g" /usr/share/nginx/html/static/js/*.js
 echo "Replaced placeholder with ${REACT_APP_BACKEND_URL_PRODUCTION}"
 
